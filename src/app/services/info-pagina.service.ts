e3bf9afbb473a4b8d1ea22cbc0c13666948f3ffe
Jorge Luis Ortiz Castillo <jorgeluisortizcastillo@gmail.com>
2019-06-26 15:48:26 -0500

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,16 +9,34 @@ export class InfoPaginaService {
 
   info: InfoPagina;
   cargada = false;
+  equipo = [];
 
   constructor(private http: HttpClient) { 
     //console.log('Servicio de info pagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
+            .subscribe(
+              (resp: InfoPagina) => {
+                this.cargada = true;
+                this.info = resp;
+                //console.log(resp);
+              }
+            );
+  }
+
+  private cargarEquipo(){
+    const url = 'https://angular-html-3d7e1.firebaseio.com/equipo.json';
+    this.http.get(url)
               .subscribe(
-                (resp: InfoPagina) => {
-                  this.cargada = true;
-                  this.info = resp;
-                  console.log(resp);
+                (res: any) => {
+                  //this.cargada = true;
+                  this.equipo = res;
+                  console.log(res);
                 }
               );
   }
