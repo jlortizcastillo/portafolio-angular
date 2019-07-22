1d396fb8f07464869115d0dd836f4295f7780df1
Jorge Luis Ortiz Castillo <jorgeluisortizcastillo@gmail.com>
2019-07-22 15:02:47 -0500

    Fin seccion 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from 'src/app/services/productos.service';
+import { PrroductoDescripcion } from 'src/app/interfaces/producto-descripcion.interface';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  producto: PrroductoDescripcion;
+  id: string;
+
+  constructor(private route: ActivatedRoute,
+              public productoService: ProductosService) { }
 
   ngOnInit() {
+    this.route.params
+              .subscribe(
+                parametros => {
+                  console.log(parametros['id']);
+                  
+                  this.productoService.getProducto(parametros['id'])
+                                      .subscribe(
+                                        (producto: PrroductoDescripcion) => {
+                                          this.id = parametros['id'];
+                                          this.producto = producto;
+                                        },
+                                        err => {
+                                          console.log(err);
+                                        }
+                                      )
+                }
+              );
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { resolve, reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -9,24 +10,70 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos(){
-    const url = 'https://angular-html-3d7e1.firebaseio.com/productos_idx.json';
+    return new Promise((resolve, reject) => {
+      const url = 'https://angular-html-3d7e1.firebaseio.com/productos_idx.json';
 
-    this.http.get(url)
-              .subscribe(
-                (res : Producto[]) => {
-                  //console.log(res);
-                  this.productos = res;
-                  this.cargando = false;
-                },
-                err => {
-                  console.log(err);
-                }
-              );
+      this.http.get(url)
+                .subscribe(
+                  (res : Producto[]) => {
+                    console.log(res);
+                    this.productos = res;
+                    this.cargando = false;
+                    resolve();
+                  },
+                  err => {
+                    console.log(err);
+                  }
+                );
+    });
+
+    
+  }
+
+  getProducto(id:string){
+    const url = `https://angular-html-3d7e1.firebaseio.com/productos/${id}.json`;
+
+    return this.http.get(url);
+  }
+
+  buscarProducto(termino: string){
+
+    if(this.productos.length === 0) {
+      //cargar productos
+      this.cargarProductos().then(() => {
+        //ejecutar despues de tener los productos
+        //aplicar el filtro
+        this.filtrarProductos(termino);
+      });
+
+    } else {
+      //aplicar el filtro
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos(termino: string){
+    console.log(this.productos);
+    
+    this.productosFiltrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(producto => {
+
+      const tituloLower = producto.titulo.toLowerCase();
+      
+      if(producto.categoria.indexOf(termino) >= 0
+         || tituloLower.indexOf(termino) >= 0) {
+          this.productosFiltrado.push(producto);
+      }
+    });
   }
 }
